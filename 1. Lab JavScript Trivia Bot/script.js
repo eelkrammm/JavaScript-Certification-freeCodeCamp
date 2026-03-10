ef6561dd520a4cf347469d1701e43a54108147a1
Adinda Puja Puspita <ppadinda7@gmail.com>
2026-03-10 11:26:22 +0700

    Create script.js

---
+++ 1. Lab JavScript Trivia Bot/script.js
@@ -0,0 +1,23 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Cortis Bot"; 
+let botLocation = "Korea"; 
+let favoriteLanguage = "python"; 
+
+console.log("My name is " + botName +" and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + "."); 
+
+let codingFact = "The fact about " + favoriteLanguage + " is it can use for data scientist and data visualization";
+
+console.log(codingFact);
+
+codingFact = "Selain itu, " + favoriteLanguage + " juga bisa digunakan untuk analisis machine learning"; 
+
+console.log(codingFact);
+
+codingFact = "You can make a snake game use " + favoriteLanguage;
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
